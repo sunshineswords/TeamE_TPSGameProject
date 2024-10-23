74b1c1f1c67b072128528c4d6677d1854873b79f
GabuGabuNoMi <139324502+IsooGabriel@users.noreply.github.com>
2024-10-23 10:00:43 +0900

    リインポート

---
+++ Assets/IsooSpace/Script/CameraRaySystem_Gabu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraRaySystem_Gabu : MonoBehaviour
+{
+    public PlayerInput playerInput;
+    public float rayDistance = 100f;
+
+    void Update()
+    {
+        if (playerInput.actions["Shot"].triggered)
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, rayDistance))
+            {
+                // Handle raycast hit
+                Debug.Log("Raycast hit: " + hit.collider.gameObject.name);
+            }
+        }
+    }
+}
