fc59e5e012d91d89b56a8ab4087a1bcc849f1c1a
riporisu <syuu.m7777@gmail.com>
2024-10-24 10:57:56 +0900

    タイマースクリプト

---
+++ Assets/MuraseSpace/Timermurase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.FPS.Gameplay;
+
+public class Timermurase: MonoBehaviour
+{
+    public PlayerCharacterController characterController;
+    
+    //カウントダウン
+    public float countdown = 5.0f;
+
+    //時間を表示するText型の変数
+    public TextMeshProUGUI timeText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //時間をカウントダウンする
+        countdown -= Time.deltaTime;
+
+        //時間を表示する
+        timeText.text = countdown.ToString("f1");
+
+        //countdownが0以下になったとき
+        if (countdown <= 0)
+        {
+            timeText.text = "時間になりました！";
+        }
+    }
+}
