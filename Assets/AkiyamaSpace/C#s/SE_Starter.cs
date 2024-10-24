ef0cc3b8a78c9a3b778753a8cc9e74de96ffe8c6
AIUUjr <j.a.2n.akym@gmail.com>
2024-10-24 09:48:58 +0900

    スクリプト１個追加

---
+++ Assets/AkiyamaSpace/C#s/SE_Starter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SE_Starter : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip ThisSE;
+    public bool Loop_Mode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
